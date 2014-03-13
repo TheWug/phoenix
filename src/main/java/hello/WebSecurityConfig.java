ebdffc3c048b7c2971fb32cf01db5bdf4fc2fbae
Ryan Schoppmeyer <rjs3275@g.rit.edu>
2014-03-13 16:38:49 -0700

    Added missing configure() definition to WSC.java don't know what I changed in Controller.java, just whitespace or something fixed logout functionality (/logout must be POST'd, not GET'd)

--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -2,12 +2,14 @@ package hello;
 
 import models.TwoFAUserDetailsService;
 import models.TwoFAUserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -46,36 +48,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     }
+    
+    @Override
+    public void configure(WebSecurity web)
+    {
+    	web.ignoring().antMatchers("/resources/**");
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //auth
-        //    .inMemoryAuthentication()
-        //        .withUser("user").password("password").roles("USER");
-
-        //auth
-        //        .inMemoryAuthentication()
-        //        .withUser("admin").password("password").roles("ADMIN");
-
-
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriverClass(org.postgresql.Driver.class);
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/auth");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
-/*
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("select user_id as principal, password as credentials, true from tn_users where username = ?")
-                .authoritiesByUsernameQuery("select user_id as principal, authority as role from tn_roles where user_id = ?")
-                .rolePrefix("ROLE_");
-                */
-
-        auth
-               .userDetailsService(userDetailsService());
-               //.jdbcAuthentication()
-                //.dataSource(dataSource);
-
+        auth.userDetailsService(userDetailsService());
     }
 
     @Bean
